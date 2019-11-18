6820d9429aeeca2777102560ed26564dde9837f0
noobstang <player.cookedchickens@gmail.com>
2019-11-18 00:47:50 -0500

    Building FXML file, corrected package names. Code 11b.

--- ICS4U-TermProject/src/com/jchan16/ics4u/tpj/Connector.java
+++ ICS4U-TermProject/src/com/jchan16/ics4u/tpj/Connector.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ics4u;
+package com.jchan16.ics4u.tpj;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
